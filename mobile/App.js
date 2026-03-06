78acec7e74c3399cc15a4f1d3a68bfde7cc3eb30
andrew041994 <andrew041994@gmail.com>
2026-03-06 00:01:14 -0500

    debug logs

--- mobile/App.js
+++ mobile/App.js
@@ -688,7 +688,7 @@ function LoginScreen({
     {
       expoClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
       iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID,
-      androidClientId: process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID,
+      androidClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
       webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
       responseType: AuthSession.ResponseType.Code,
       usePKCE: true,
